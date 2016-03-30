afdc7fd1ca83561bee1f47da9d7790c23ad0d230
daryljones541 <daryljones530@sbcglobal.net>
2016-03-30 08:35:52 -0700

    Fresh project with navbar, Pictures folder, and Donate button

---
+++ Strek4Mayor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Strek4Mayor.MvcApplication" Language="C#" %>
---
+++ Strek4Mayor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Strek4Mayor.Startup))]
+namespace Strek4Mayor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
