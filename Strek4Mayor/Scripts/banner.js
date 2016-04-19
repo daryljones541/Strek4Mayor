80be4788bf27c4147e33eef3ca838bd55d367635
daryljones541 <daryljones530@sbcglobal.net>
2016-04-19 09:14:42 -0700

    Improved shared layout's JavaScript measure function

--- Strek4Mayor/Scripts/banner.js
+++ Strek4Mayor/Scripts/banner.js
@@ -7,9 +7,13 @@ $(window).resize(function () {
 });
 
 function measure() {
+    var windowWidth = $(window).width();
     var donateWidth = $('#right-panel').width();
     $('#right-panel img').width(donateWidth);
+    var rightWidth = $('#right-panel').outerWidth();
+    var leftWidth = .94 * (windowWidth - rightWidth);
     var panelHeight = $('#right-panel').outerHeight();
+    $('#left-panel').width(leftWidth);
     $('#left-panel').height(panelHeight);
     $('#left-panel img').height(panelHeight);
     var navbarHeight = $('#navbar').outerHeight();
