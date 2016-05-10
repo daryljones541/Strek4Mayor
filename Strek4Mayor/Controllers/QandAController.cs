a8590fef8a5e81a4be24bb606cb225acfa31e882
hunterthornton <hunter.thornton@gmail.com>
2016-05-10 13:23:26 -0700

    Added error message to captcha

--- Strek4Mayor/Controllers/QandAController.cs
+++ Strek4Mayor/Controllers/QandAController.cs
@@ -94,7 +94,8 @@ namespace Strek4Mayor.Controllers
             }
             else
             {
-                MvcCaptcha.ResetCaptcha("Incorrect CAPTCHA code!");
+                MvcCaptcha.IsCaptchaSolved("Incorrect CAPTCHA code!");
+                ViewBag.ErrMessage = "Error: captcha is not valid."; 
             }
 
             return View(qanda);
